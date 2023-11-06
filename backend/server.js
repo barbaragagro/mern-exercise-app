d665a37368612de85586a27d4b8e0d1b16d0fc04
barbaragagro <barbaragagro1@gmail.com>
2023-11-06 18:51:57 +0100

    creating simple routes

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,19 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import workoutRoutes from './routes/workouts.js';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get('/', (req, res) => {
-  res.json({ mssg: 'Welcome to the app' });
-});
+app.use('/api/workouts', workoutRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log('listening on port', process.env.PORT);
