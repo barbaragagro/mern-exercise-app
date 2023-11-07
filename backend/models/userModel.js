2404cb6bd6c466236fc9dec76b9d9f82e64055ca
barbaragagro <barbaragagro1@gmail.com>
2023-11-07 16:46:25 +0100

    error handling for sign up functionality

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import validator from 'validator';
 
 const Schema = mongoose.Schema;
 
@@ -16,6 +17,15 @@ const userSchema = new Schema({
 });
 
 userSchema.statics.signup = async function (email, password) {
+  if (!email || !password) {
+    throw Error('All fields must be filled');
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid');
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough');
+  }
   const exists = await this.findOne({ email });
 
   if (exists) {
